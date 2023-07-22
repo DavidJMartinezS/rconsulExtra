bc76afd1b81f8900b2262407acc42ef7f526e4b0
DavidJMartinezS <david.tinez1@gmail.com>
2023-07-22 11:34:23 -0400

    info crs gajardo

--- R/Formaciones_Gajardo.R
+++ R/Formaciones_Gajardo.R
@@ -1,12 +1,16 @@
 #' Formaciones Gajardo
 #'
-#' Formaciones vegetales descritas por Gajardo (1994)
+#' Formaciones vegetales descritas por Gajardo (1994).
+#' CRS: EPSG:4326. WGS 84 -- WGS84 - World Geodetic System 1984
+#' Unit: degree
+#' Coordinate system: Ellipsoidal 2D CS. Axes: latitude, longitude. Orientations: north, east. UoM: degree
+#' proj4: "+proj=longlat +datum=WGS84 +no_defs +type=crs"
 #'
 #' @format ## `Formaciones_Gajardo`
 #' Simple feature collection with 939 features and 8 fields:
 #' \describe{
 #'   \item{Formacion, Subregion, Region, Distrito, Provincia}{Detalle de las Formaciones a diferentes escalas}
-#'   \item{Sup_km2, Leng_km}{Área y longitud del polígono de la formación}
+#'   \item{Area_km2, Length_km}{Área y longitud geodésica del polígono de la formación}
 #' }
 #' @source <https://www.ide.cl/index.php/flora-y-fauna/item/1515-formaciones-vegetacionales-gajardo>
 "Formaciones_Gajardo"
